6eb4f9c90d9fc6b42e7dcbb2ced6056630b4197a
Corina Andariese <candariese@gmail.com>
2018-08-25 15:58:13 -0700

    Added Compose form

--- src/components/ComposeForm.jsx
+++ src/components/ComposeForm.jsx
@@ -11,20 +11,20 @@ const ComposeForm = (props) => {
   }
 return (
   <form className={`form-horizontal well ${props.composing ? '' : 'hidden'}`} onSubmit={submitMessage}>
-  <div className="form-group">
-    <div className="col-sm-8 col-sm-offset-2">
-      <h4>Compose Message</h4>
+    <div className="form-group">
+      <div className="col-sm-8 col-sm-offset-2">
+        <h4>Compose Message</h4>
+      </div>
     </div>
-  </div>
-  <div className="form-group">
-    <label for="subject" className="col-sm-2 control-label">Subject</label>
-    <div className="col-sm-8">
-      <input type="text" className="form-control" id="subject" placeholder="Enter a subject" name="subject">
-      </input>
+    <div className="form-group">
+      <label htmlFor="subject" className="col-sm-2 control-label">Subject</label>
+      <div className="col-sm-8">
+        <input type="text" className="form-control" id="subject" placeholder="Enter a subject" name="subject">
+        </input>
+      </div>
     </div>
-  </div>
-  <div className="form-group">
-    <label for="body" className="col-sm-2 control-label">Body</label>
+    <div className="form-group">
+      <label htmlFor="body" className="col-sm-2 control-label">Body</label>
     <div className="col-sm-8">
       <textarea name="body" id="body" className="form-control"></textarea>
     </div>
